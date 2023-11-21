df9ae095d09c94dd549d7b1dbeb06cc1cdbbf814
Rishabh Sharma <113721437+therishabhsharma03@users.noreply.github.com>
2023-11-22 00:28:31 +0530

    Update config.js

--- src/component/config.js
+++ src/component/config.js
@@ -1,4 +1,5 @@
 // config.js
-const API_BASE_URL = "http://localhost:4000"; // Replace with your actual base URL
-
+const API_BASE_URL = "https://tasks-6s1c.onrender.com"; // Replace with your actual base URL
+// http://localhost:4000
+// https://tasks-6s1c.onrender.com
 export default API_BASE_URL;
